0f7a16b2d0c70c9411ec4f79d380aa7da718d36c
Timothy Washington <twashing@interruptsoftware.com>
2020-05-20 18:52:26 -0400

    Add Highstock graphing library

--- App.js
+++ App.js
@@ -4,25 +4,62 @@ import {
   Appbar, Avatar, Button, Card, Title, Paragraph,
   Provider as PaperProvider
 } from 'react-native-paper';
-
-
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+import Highcharts from 'highcharts/highstock';
 
 
 const socket = new WebSocket("ws://localhost:8080/ws");
 
+
+/* Navigation Screens
+
+Start with simple Stack Navigator
+
+Index
+Dashboard
+Settings
+Game */
+
+const getSeries = () => {
+  return Highcharts.charts[0].series[0];
+};
+
 export default class App extends Component {
 
   componentDidMount() {
+
     console.log("componentDidMount CALLED");
 
+    // A. Add Highstock Chart
+    Highcharts.chart('highStock', {
+      "rangeSelector": {
+        "selected": 1
+      },
+      "title": {
+        "text": "Sine Wave Stock Price"
+      },
+      "series": [
+        {
+          "name": "Sine Wave",
+          "data": [],
+          "tooltip": {
+            "valueDecimals": 2
+          }
+        }
+      ]
+    });
+
+    // B. WebSocket
     socket.addEventListener('open', function (event) {
       console.log("Websocket client connected");
       socket.send('Hello Server!');
     });
 
     socket.addEventListener('message', function (event) {
-      console.log('Message from server ', event.data);
+
+      // console.log('Message from server ', event.data);
+      let data = JSON.parse([event.data]);
+      console.log(data);
+      getSeries().addPoint(data, true, false);
     });
   }
 
@@ -41,7 +78,9 @@ export default class App extends Component {
             <Title>IBM</Title>
             <Paragraph>American Blue Chip company</Paragraph>
           </Card.Content>
-          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+
+          <div id="highStock"/>
+
           <Card.Actions>
             <Button>Cancel</Button>
             <Button>Ok</Button>
@@ -52,33 +91,6 @@ export default class App extends Component {
     );
   }
 }
-
-/* export default function App() {
-  
-  return (
-    <PaperProvider>
-
-      <Appbar.Header >
-        <Appbar.Content title="Beat The Market" subtitle="Can you... beat the market ?!" />
-        <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-        <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
-      </Appbar.Header>
-
-      <Card>
-        <Card.Content>
-          <Title>IBM</Title>
-          <Paragraph>American Blue Chip company</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions>
-      </Card>
-
-    </PaperProvider>
-  );
-}*/
 
 const styles = StyleSheet.create({
   bottom: {
