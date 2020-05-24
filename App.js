6da65a98c0285bc63ef45139b63faa529cc9ebe0
Timothy Washington <twashing@interruptsoftware.com>
2020-05-24 16:14:53 -0400

    Adding react-navigation native + stack libs Initial navigation between 4 sample screens

--- App.js
+++ App.js
@@ -1,102 +1,77 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {
+import { Button, View } from 'react-native';
+/* import {
   Appbar, Avatar, Button, Card, Title, Paragraph,
   Provider as PaperProvider
-} from 'react-native-paper';
-import Highcharts from 'highcharts/highstock';
+} from 'react-native-paper';*/
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-const socket = new WebSocket("ws://localhost:8080/ws");
-
-
-/* Navigation Screens
-
-Start with simple Stack Navigator
-
-Index
-Dashboard
-Settings
-Game */
-
-const getSeries = () => {
-  return Highcharts.charts[0].series[0];
-};
-
-export default class App extends Component {
-
-  componentDidMount() {
-
-    console.log("componentDidMount CALLED");
-
-    // A. Add Highstock Chart
-    Highcharts.chart('highStock', {
-      "rangeSelector": {
-        "selected": 1
-      },
-      "title": {
-        "text": "Sine Wave Stock Price"
-      },
-      "series": [
-        {
-          "name": "Sine Wave",
-          "data": [],
-          "tooltip": {
-            "valueDecimals": 2
-          }
-        }
-      ]
-    });
-
-    // B. WebSocket
-    socket.addEventListener('open', function (event) {
-      console.log("Websocket client connected");
-      socket.send('Hello Server!');
-    });
-
-    socket.addEventListener('message', function (event) {
-
-      // console.log('Message from server ', event.data);
-      let data = JSON.parse([event.data]);
-      console.log(data);
-      getSeries().addPoint(data, true, false);
-    });
-  }
-
-  render() {
-    return (
-      <PaperProvider>
-
-        <Appbar.Header >
-          <Appbar.Content title="Beat The Market" subtitle="Can you... beat the market ?!" />
-          <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-          <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
-        </Appbar.Header>
-
-        <Card>
-          <Card.Content>
-            <Title>IBM</Title>
-            <Paragraph>American Blue Chip company</Paragraph>
-          </Card.Content>
-
-          <div id="highStock"/>
-
-          <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-          </Card.Actions>
-        </Card>
-
-      </PaperProvider>
-    );
-  }
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  bottom: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Notifications"
+        onPress={() => navigation.navigate('Notifications')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const linking = {
+  prefixes: ['http://localhost', 'localhost://'],
+};
+
+export default function App() {
+  return (
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <MyStack/>
+    </NavigationContainer>
+  );
+}
