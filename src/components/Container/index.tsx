c6923646c765f3f4c16335757c4aa5b70c893712
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-08-05 17:22:04 +0300

    Updated Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,15 +1,16 @@
 import React, { ReactNode } from 'react';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import { styles } from './styles';
 
 interface Props {
   children: ReactNode | ReactNode[];
+  style?: ViewStyle;
   testID?: string;
 }
 
-export function Container({ children, testID }: Props) {
+export function Container({ children, style: propStyle, testID }: Props) {
   return (
-    <View style={styles.center} testID={testID}>
+    <View style={[styles.center, propStyle]} testID={testID}>
       {children}
     </View>
   );
