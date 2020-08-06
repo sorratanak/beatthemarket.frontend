ad1d3dc7becff84e6f530c5faf1b9771e634506b
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-08-06 14:24:37 +0300

    Refactored themes, added ChartHeader component to GameChartBoard

--- src/components/GameChartBoard/styles.ts
+++ src/components/GameChartBoard/styles.ts
@@ -1,5 +1,30 @@
 import { StyleSheet } from 'react-native';
+import { ITheme } from '../../themes/interface';
 
-export const styles = StyleSheet.create({
-  container: {},
-});
+export const getThemedStyles = (theme: ITheme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    /* Chart area */
+    chartArea: {
+      backgroundColor: theme.GAME_SCREEN.VIEW_COLOR,
+    },
+
+    /* Chart header */
+    chartHeaderContainer: {},
+    chartHeaderSubcontainer: {},
+    chartHeaderImageContainer: {},
+    chartHeaderTitle: {},
+    chartHeaderTitleAbbr: {},
+
+    /* Chart */
+    chartContainer: {},
+
+    /* Footer */
+    infoArea: {},
+  });
---
+++ src/themes/commonStyles.ts
@@ -0,0 +1,16 @@
+import { Platform } from 'react-native';
+import { COLORS } from './colors';
+
+// Use it inside StyleSheet.create() only
+export const COMMON_STYLES = {
+  SHADOW_BOX: Platform.select({
+    ios: {},
+    android: {},
+    web: {
+      shadowOpacity: 0.1,
+      shadowRadius: 15,
+      shadowColor: COLORS.BLACK,
+      shadowOffset: { height: 0, width: 0 },
+    },
+  }),
+};
