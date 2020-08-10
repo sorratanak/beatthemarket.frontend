bb00bac966a7f27530d536d084082719c93942dd
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-08-10 15:42:45 +0300

    Add chart random color when activeStock has been changed

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect, useContext } from 'react';
 import { Dimensions } from 'react-native';
 import _ from 'lodash';
+import pickRandom from 'pick-random';
 
 import {
   VictoryChart,
@@ -12,6 +13,7 @@ import {
 import { COLORS } from '../../themes/colors';
 import { IPoint } from '../../types';
 import { ANIMATION_OPTIONS } from './config';
+import { GameContext } from '../../contexts';
 
 const WINDOW = Dimensions.get('window');
 const CHART_HEIGHT = WINDOW.height * 0.6;
@@ -21,12 +23,29 @@ interface Props {
   data: IPoint[];
 }
 export function LineChart({ data }: Props) {
+  const { activeStock } = useContext(GameContext);
+
   const [dynamicYDomainMin, dynamicYDomainMax] = useMemo(() => {
     return [
       _.minBy(data, (el) => el.y)?.y - 0.5,
       _.maxBy(data, (el) => el.y)?.y + 0.5,
     ];
   }, [data]);
+
+  const [chartColor, setChartColor] = useState(null);
+
+  useEffect(() => {
+    setChartColor(
+      pickRandom([
+        COLORS.BILBAO,
+        COLORS.VIKING,
+        COLORS.EBONY_CLAY,
+        COLORS.CORNFLOWER_BLUE,
+        COLORS.VALENCIA,
+        COLORS.MACARONI_AND_CHEESE,
+      ])[0],
+    );
+  }, [activeStock, setChartColor]);
 
   return (
     <VictoryChart
@@ -52,13 +71,13 @@ export function LineChart({ data }: Props) {
       <VictoryLine
         // TODO dynamic getData by zoom domain
         data={data}
-        style={{ data: { stroke: COLORS.CORNFLOWER_BLUE } }}
+        style={{ data: { stroke: chartColor } }}
         interpolation="cardinal"
         animate={ANIMATION_OPTIONS}
       />
       <VictoryScatter
         data={data}
-        style={{ data: { fill: COLORS.CORNFLOWER_BLUE } }}
+        style={{ data: { fill: chartColor } }}
         size={3}
         animate={ANIMATION_OPTIONS}
       />
