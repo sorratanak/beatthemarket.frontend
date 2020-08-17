e3f18a0e86638badb5348e270c1fc096b85762bc
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-08-17 18:33:02 +0300

    Added settings PNG icons, refactored assets/index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,15 @@
+/* eslint-disable global-require */
+
 export const IMAGES = {
+  /* Logos */
   GOOGLE: require('./logo/google.png'),
   FACEBOOK: require('./logo/facebook.png'),
   APPLE: require('./logo/apple.png'),
+
+  /* Settings */
+  FLAG: require('./settings/redFlag.png'),
+  FIRE: require('./settings/fire.png'),
+  SPEAKER: require('./settings/speaker.png'),
+  HEADPHONES: require('./settings/headphones.png'),
+  USER: require('./settings/user.png'),
 };
