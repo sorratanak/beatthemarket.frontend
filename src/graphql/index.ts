5d2388e39dd24d4263f46ded9978d80134e641f4
Artem <arte.tkachuk@gmail.com>
2020-08-27 16:18:46 +0300

    local ip integration

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,7 +5,7 @@ import {
   ApolloLink,
   split,
 } from '@apollo/client';
-// import { Platform } from 'react-native';
+import { Platform } from 'react-native';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { setContext } from '@apollo/client/link/context';
@@ -17,13 +17,13 @@ import {
   DEVELOPMENT_SERVER_PORT,
 } from '../constants';
 
-// const ip = Platform.OS === 'android' ? '10.0.2.2' : '127.0.0.1';
+const ip = Platform.OS === 'android' ? '10.0.2.2' : '127.0.0.1';
 
-// export const SERVER_API_URL = `http://${ip}:8080/api`;
-// export const SERVER_WS_URL = `ws://${ip}:8080/ws`;
+export const SERVER_API_URL = `http://${ip}:8080/api`;
+export const SERVER_WS_URL = `ws://${ip}:8080/ws`;
 
-export const SERVER_API_URL = `http://${DEVELOPMENT_SERVER_BASE_IP}:${DEVELOPMENT_SERVER_PORT}/api`;
-export const SERVER_WS_URL = `ws://${DEVELOPMENT_SERVER_BASE_IP}:${DEVELOPMENT_SERVER_PORT}/ws`;
+// export const SERVER_API_URL = `http://${DEVELOPMENT_SERVER_BASE_IP}:${DEVELOPMENT_SERVER_PORT}/api`;
+// export const SERVER_WS_URL = `ws://${DEVELOPMENT_SERVER_BASE_IP}:${DEVELOPMENT_SERVER_PORT}/ws`;
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
