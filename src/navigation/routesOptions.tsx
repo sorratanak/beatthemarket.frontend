60c1870617fba2c7daf1bdd988fbea8e6e6b8568
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-09-01 16:55:36 +0300

    Added themedStyles for navigation header [mobile]

--- src/navigation/routesOptions.tsx
+++ src/navigation/routesOptions.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import { ITheme } from '../themes/interface';
 import { IMAGES } from '../assets';
+import { LIGHT_THEME } from '../themes';
 
 export const withoutHeaderOptions = { headerShown: false };
+
+export const getThemedHeaderOptions = (theme: ITheme) => ({
+  headerStyle: {
+    backgroundColor: theme.DEFAULT.SECONDARY_BACKGROUND_COLOR,
+    shadowRadius: 0,
+    shadowOffset: {
+      height: 0,
+    },
+  },
+  headerTintColor: theme.DEFAULT.TEXT_COLOR,
+});
 
 export const getThemedTabBarOptions = (theme: ITheme) => ({
   activeTintColor: theme.MENU.ACTIVE_LABEL_COLOR,
@@ -12,6 +24,7 @@ export const getThemedTabBarOptions = (theme: ITheme) => ({
     fontSize: 10,
   },
   style: {
+    borderTopWidth: theme === LIGHT_THEME ? 1.5 : 0,
     backgroundColor: theme.DEFAULT.SECONDARY_BACKGROUND_COLOR,
   },
 });
