9da3d1d777753aace06c3a64e13b9ddb054cc296
Artem Tkachuk <arte.tkachuk@gmail.com>
2020-09-01 18:02:22 +0300

    Added ExtraSubscription screen [Mobile]

--- src/screens/ExtraSubscriptions/styles.ts
+++ src/screens/ExtraSubscriptions/styles.ts
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 import { ITheme } from '../../themes/interface';
+import { COLORS } from '../../themes/colors';
 
 export const getThemedStyles = (theme: ITheme) =>
   StyleSheet.create({
@@ -9,7 +10,38 @@ export const getThemedStyles = (theme: ITheme) =>
       backgroundColor: theme.DEFAULT.PRIMARY_BACKGROUND_COLOR,
     },
     contentContainer: {
-      padding: 0,
       paddingVertical: 16,
+    },
+    description: {
+      marginTop: 24,
+      color: theme.DEFAULT.TEXT_COLOR,
+      fontSize: 14,
+      lineHeight: 20,
+      letterSpacing: 0.41,
+    },
+    buttonContainer: {
+      marginTop: 24,
+      alignSelf: 'center',
+      paddingHorizontal: 96,
+      backgroundColor: theme.SUBSCRIPTIONS_SCREEN.BUTTON_BACKGROUND_COLOR,
+    },
+    selectedContainer: {
+      paddingTop: 13,
+      paddingBottom: 41,
+      borderRadius: 7,
+      backgroundColor: COLORS.MOON_RAKER,
+    },
+    selectedValue: {
+      fontSize: 20,
+      lineHeight: 24,
+      textAlign: 'center',
+      marginBottom: 18,
+    },
+    selectedDescription: {
+      textAlign: 'center',
+      fontSize: 13,
+      lineHeight: 16,
+      fontWeight: '300',
+      color: COLORS.GRAY,
     },
   });
